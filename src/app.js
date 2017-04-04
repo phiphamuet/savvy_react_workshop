ba8d1d38af010cc627f724023cebdfe924f69950
PHI PHAM <phiphamuet@gmail.com>
2017-04-05 00:19:34 +0700

    update stateless component

--- src/app.js
+++ src/app.js
@@ -1,18 +1,77 @@
 import React, { Component } from 'react';
 import LoginForm from './components/loginform';
+import axios from 'axios';
 class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            user: {}
+            user: {},
+            loginForm: {}
         };
+        this.onLoginSubmit = this.onLoginSubmit.bind(this);
+        this.onLogoutButtonClick = this.onLogoutButtonClick.bind(this);
+        this.handleChangeCredential = this.handleChangeCredential.bind(this);
     }
+
+    componentDidMount() {
+        try {
+            const userCredentials = localStorage.getItem('user');
+            const user = JSON.parse(userCredentials);
+            if (user) {
+                this.setState({
+                    user
+                });
+            }
+        } catch (e) {
+            console.log(e);
+        }
+    }
+    
+
+    onLoginSubmit() {
+        axios.post('http://localhost:1337/customer/authcustomer/login', {
+            'email': this.state.loginForm.email,
+            'password': this.state.loginForm.password
+        })
+        .then(({ data: body }) => {
+            this.setState({
+                user: body.data
+            });
+            localStorage.setItem('user', JSON.stringify(body.data));
+        })
+        .catch(error => {
+            console.log(error)
+        });
+    }
+
+    handleChangeCredential(event, key) {
+        const loginForm = Object.assign(this.state.loginForm, {
+            [key]: event.target.value
+        });
+        this.setState({
+            loginForm
+        });
+    }
+
+    onLogoutButtonClick() {
+        this.setState({
+            user: {}
+        });
+        localStorage.removeItem('user');
+    }
+
 
     render() {
         return (
             <div>
-                <h1>Welcome to {this.state.user.name} React Workshop in SavvyCom</h1>
-                <LoginForm/>
+                <h1>Hi {this.state.user.user && this.state.user.user.name || 'Anonymous'}, Welcome to React Workshop in SavvyCom</h1>
+                <h1>{!this.state.user.user && 'Login to join workshop'}</h1>
+                <LoginForm
+                    user = {this.state.user}
+                    onLoginSubmit = {this.onLoginSubmit}
+                    onLogoutButtonClick = {this.onLogoutButtonClick}
+                    handleChangeCredential = {this.handleChangeCredential}
+                />
             </div>
         )
     }
--- src/components/loginform/index.js
+++ src/components/loginform/index.js
@@ -1,85 +1,27 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class LoginForm extends Component {
-
-    constructor(props) {
-        super(props);
-        this.onLoginSubmit = this.onLoginSubmit.bind(this);
-        this.onLogoutButtonClick = this.onLogoutButtonClick.bind(this);
-        this.state = {
-        }
-    }
-
-    componentDidMount() {
-        try {
-            const userData = localStorage.getItem('user');
-            if (userData) {
-                const user = JSON.parse(userData);
-                this.setState({ user });
-            }
-        } catch (e) {
-            console.log(e);
-        }
-    }
-    
-
-    onLoginSubmit() {
-        var myHeaders = new Headers();
-        var payload = JSON.stringify({
-            'email': this.email.value,
-            'password': this.password.value
-        });
-        myHeaders.set("Content-Type", "application/json");
-        fetch('http://localhost:1337/customer/authcustomer/login', {
-            mode: 'cors',
-            method: 'POST',
-            headers: myHeaders,
-            body: payload
-        })
-        .then(response => {
-            return response.json();
-        })
-        .then(body => {
-            this.setState({
-                user: body.data
-            });
-            localStorage.setItem('user', JSON.stringify(body.data));
-        })
-        .catch(error => {
-            console.log(error)
-        });
-    }
-
-    onLogoutButtonClick() {
-        this.setState({
-            user: ''
-        });
-        localStorage.removeItem('user');
-    }
-
-    render() {
-        if (this.state.user) {
-            return (
-                <div>
-                    <h1>You are logged in</h1>
-                    <a href="javascript:;">
-                        <button onClick={() => {this.onLogoutButtonClick()}}>Logout</button>
-                    </a>
-                </div>
-            )
-        }
+const LoginForm = ({user, onLogoutButtonClick, onLoginSubmit,
+    handleChangeCredential}) => {
+    if (user.token) {
         return (
             <div>
-                <label htmlFor="email">Email</label>
-                <input id="email" type="text" ref={email => this.email = email}/>
-                <br/>
-                <label htmlFor="password">Password</label>
-                <input id="password" type="password" ref={password => this.password = password}/>
-                <br/>
-                <button onClick={() => this.onLoginSubmit()}>Login</button>
+                <a href="javascript:;">
+                    <button onClick={() => {onLogoutButtonClick()}}>Logout</button>
+                </a>
             </div>
-        );
+        )
     }
-}
+    return (
+        <div>
+            <label htmlFor="email">Email</label>
+            <input id="email" type="text" onChange={(e) => handleChangeCredential(e, 'email')}/>
+            <br/>
+            <label htmlFor="password">Password</label>
+            <input id="password" type="password" onChange={(e) => handleChangeCredential(e, 'password')}/>
+            <br/>
+            <button onClick={() => onLoginSubmit()}>Login</button>
+        </div>
+    );
+};
 
 export default LoginForm;
