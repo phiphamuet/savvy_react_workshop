193839be61d839e0816903bb71d146a10f3c0ce6
PHI PHAM <phipham@PHIs-MacBook-Air.local>
2017-04-04 16:55:37 +0700

    init project

---
+++ src/app.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Welcome to React Workshop in SavvyCom</h1>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(
+    <App/>,
+    document.getElementById("app")
+);
---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        filename: 'app.js',
+        path: path.resolve(__dirname, 'bundle')
+    },
+    devtool: 'source-map',
+    module: {
+        loaders: [
+            {
+                test: /\.js?$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            }
+        ]
+    }
+}
